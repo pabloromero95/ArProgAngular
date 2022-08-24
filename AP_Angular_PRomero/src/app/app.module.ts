feda92623ef111e9cce1b68f61735c3c40518ab2
PabloRomero <pabloromeroenjamio@gmail.com>
2022-08-24 09:56:50 -0300

    Modularized APFront Proyect

--- AP_Angular_PRomero/src/app/app.module.ts
+++ AP_Angular_PRomero/src/app/app.module.ts
@@ -1,9 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import { BodyComponent } from './body/body.component';
-import { NavbarComponent } from './navbar/navbar.component';
 import { FlagComponent } from './flag/flag.component';
 import { AboutmeComponent } from './aboutme/aboutme.component';
 import { ProyectsComponent } from './proyects/proyects.component';
@@ -14,8 +11,6 @@ import { FooterComponent } from './footer/footer.component';
 @NgModule({
   declarations: [
     AppComponent,
-    BodyComponent,
-    NavbarComponent,
     FlagComponent,
     AboutmeComponent,
     ProyectsComponent,
